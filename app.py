5fc56726005d399c60a75f8ca9deb5e11e75d435
dataheadway <dataheadway5@gmail.com>
2024-10-24 17:11:26 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ VERIFY_TOKEN = "my_secure_token_123"
 # Your Instagram App credentials
 CLIENT_ID = "556879356850932"
 CLIENT_SECRET = "a33c58e9c6ddc8e9e5e60080ea4997ea"
-REDIRECT_URI = "https://cbs-beta.vercel.app/instagram/callback"
+REDIRECT_URI = "https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=1195291211766740&redirect_uri=https://cbs-dataheadways-projects.vercel.app/instagram/callback&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish"
 GRAPH_API_URL = "https://graph.facebook.com/v17.0"
 ACCESS_TOKEN = "EAAH6epQZCcvQBOzwsVGXKBqx2kPtERmSBLFShZC2Xo1JpkowcXAmX25tZAZAEJIlxoIZCA2Ndoho7VXFzzQo3SB7RWBZChTmxetk13Bo9CNfBNnQ6WoOmGczyqiu1SZCL2Ln9XbECjZAv6wgw8fdkc3VuUsNs91MNtAxtryblwQEpwsZAVQ6ZCv4YQWWH47XZB6Cv0kN"  # Replace with the obtained access token
 
@@ -64,7 +64,7 @@ def instagram_callback():
             'client_id': CLIENT_ID,
             'client_secret': CLIENT_SECRET,
             'grant_type': 'authorization_code',
-            'redirect_uri': 'https://cbs-beta.vercel.app/instagram/callback',
+            'redirect_uri': REDIRECT_URI,
             'code': code
         }
         
